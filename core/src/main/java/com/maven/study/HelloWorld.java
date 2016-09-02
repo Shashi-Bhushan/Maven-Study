98923b434d602fb8015581491eb88b8a2d80bcf3
Shashi Bhushan <shashib@intelligrape.com>
2016-09-02 17:50:14 +0530

    Enabling Groovy on Maven Project

---
+++ core/src/main/java/com/maven/study/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.maven.study;
+
+public class HelloWorld {
+
+	public static void main(String... arg){
+		System.out.println("Hello World");
+	}
+}
---
+++ core/src/main/java/com/maven/study/HelloWorldGroovy.groovy
@@ -0,0 +1,8 @@
+package com.maven.study
+
+/**
+ * @author Shashi Bhushan
+ *          Created on 2/9/16.
+ *          For Project : Maven-Study 
+ */
+println "Hello"
